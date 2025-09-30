c0a86aacd42893ed3171171e8e0bd013997f9a9d
Aasim Attia <aasim.atya@gulfterminal.net>
2025-09-30 10:37:53 +0300

    initial setup

--- script.js
+++ script.js
@@ -253,7 +253,7 @@ function initializeMapTabs() {
         // Update content
         contentCard.innerHTML = `
           <div class="tab-icon-container">
-            <img src="10607569.gif" alt="Loading" class="tab-gif-icon" />
+            <img src="images/10607569.gif" alt="Loading" class="tab-gif-icon" />
           </div>
           
           <p class="tab-message-text">${message}</p>
